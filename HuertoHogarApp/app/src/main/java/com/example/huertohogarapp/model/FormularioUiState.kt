7644e17a33fd9d76399c038bcb4d39324d90e64b
karlabelen012 <ka.herrerah@duocuc.cl>
2025-11-05 00:20:50 -0300

    Delete HuertoHogarApp directory

--- HuertoHogarApp/app/src/main/java/com/example/huertohogarapp/model/FormularioUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.huertohogarapp.model
-
-data class FormularioUiState(
-    val nombre: String = "",
-    val correo: String = "",
-    val errores: ErroresFormulario = ErroresFormulario(),
-    val enviado: Boolean = false              // indica si el formulario fue enviado
-)
-
