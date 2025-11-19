ff3d60d4f94fefab4499da9850e8981066c4835d
karlabelen012 <ka.herrerah@duocuc.cl>
2025-11-19 03:52:09 -0300

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("com.android.application") version "8.13.1" apply false
-    id("com.android.library") version "8.13.1" apply false
-    // 🔧 aquí forzamos Kotlin 1.9.25 (compatible con Compose Compiler 1.5.15)
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-}
